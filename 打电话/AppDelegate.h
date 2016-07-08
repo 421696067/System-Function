fb10642d0cc60f2ed0c0d1aff380e8b5bf831b30
lipanxiang <421696067@qq.com>
2016-07-08 13:41:52 +0800

    Initial Commit

---
+++ 打电话/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  打电话
+//
+//  Created by 李攀祥 on 16/7/8.
+//  Copyright © 2016年 李攀祥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
